7e3f77c1fecee6cc63fff0bedb9793b9c300b783
m1ch3l99 <60483510+m1ch3l99@users.noreply.github.com>
2021-06-05 19:28:37 +0300

    Update patterns.js

--- src/patterns/patterns.js
+++ src/patterns/patterns.js
@@ -1,4 +1,4 @@
-const phonePattern = /^((961*(3|7(0|1)))|(03|7(0|1)))\d{6}$/g;
+const phonePattern = /^((961*(3|8(1)|7(0|1|6)))|(03|7(0|1)))\d{6}$/g;
 const emailPattern =
   /^([a-z\d\\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/g;
 
